6720e3324dfe18c78f507101c1f8a4351d873162
alexandre.legoff <alexandre.legoff@interalia.net>
2017-10-05 13:26:41 -0500

    cambio

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 $(document).ready(function(){
+
+
+	var activeLi = 0;
+
+	setInterval(function(){ 
+		changeLi();
+	}, 3000);
+
+	function changeLi(){
+		$("#background_video .video_txt > div").removeClass('active');
+		activeLi++;
+		if ( activeLi == 3) {
+			activeLi = 0;
+		}
+		$("#background_video .video_txt > div").eq(activeLi).addClass('active');
+	}
 });
